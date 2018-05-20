f86a13848a2bf6b559b05f1c9e5438ef53710fb9
Glenn Mesel <glennmesel@gmail.com>
2018-05-20 23:42:14 +0200

    Fixed so you can uninstall in Programs and functions.

--- scripts/install.bat
+++ scripts/install.bat
@@ -5,5 +5,11 @@ echo.
 echo.
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 rd /q /s "%programfiles%\DiscLovestory"
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /f
 mkdir "%programfiles%\DiscLovestory"
 xcopy "%~dp0..\scripts" "%programfiles%\DiscLovestory" /s
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /v DisplayName /t REG_SZ /d DiscLovestory /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /v InstallLocation /t REG_SZ /d "%programfiles%\DiscLovestory"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /v UninstallString /t REG_SZ /d "%programfiles%\DiscLovestory\uninstall.bat"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /v Publisher /t REG_SZ /d "Glenn Mesel"
---
+++ scripts/uninstall.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Uninstalling DiscLovestory
+echo.
+echo.
+echo.
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
+rd /q /s "%programfiles%\DiscLovestory"
+
+echo Fixing regedit
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DiscLovestory /f
