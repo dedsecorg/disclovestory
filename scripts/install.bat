2a1c7ea898e2597b3ccf3cd45d413607c3cdc3e9
Glenn Mesel <glennmesel@gmail.com>
2018-05-20 22:45:17 +0200

    install.bat is now working.

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,2 +1,9 @@
+@echo off
+echo Installing DiscLovestory
+echo.
+echo.
+echo.
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
-xcopy %~dp0 c:\script\
+rd /q /s "%programfiles%\DiscLovestory"
+mkdir "%programfiles%\DiscLovestory"
+xcopy "%~dp0..\scripts" "%programfiles%\DiscLovestory" /s
