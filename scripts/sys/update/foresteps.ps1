0f38c92d56b39a7de0119d6b981d31cacb8522bb
Glenn Mesel <glennmesel@gmail.com>
2018-05-29 09:10:26 +0200

    made it easier to reuse.

--- scripts/sys/update/foresteps.ps1
+++ scripts/sys/update/foresteps.ps1
@@ -7,6 +7,6 @@ if (Test-Path $FileName)
   Remove-Item $FileName
 }
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-wget -O "$env:temp\Disclovestory\DiscLovestory.master.zip" https://github.com/godzillamesel/disclovestory/archive/master.zip
+wget -O "$env:temp\Disclovestory\$FileName" https://github.com/godzillamesel/disclovestory/archive/master.zip
 mkdir $env:temp\DiscLovestory\unzip
-Expand-Archive $env:temp\Disclovestory\DiscLovestory.master.zip -DestinationPath $env:temp\DiscLovestory\unzip
+Expand-Archive $env:temp\Disclovestory\$FileName -DestinationPath $env:temp\DiscLovestory\unzip
